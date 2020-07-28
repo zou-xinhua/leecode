2c3d393803926ec761c5be817fa8bd2ec6a9438a
邹欣华 <hzzouxinhua@corp.netease.com>
2020-07-28 19:41:39 +0800

    322.零钱兑换

---
+++ 322.零钱兑换.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=322 lang=javascript
+ *
+ * [322] 零钱兑换
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    let dp = new Array(amount + 1).fill(amount + 1)
+    dp[0] = 0
+    for(let i = 0; i < amount + 1; i++) {
+        for(let corn of coins) {
+            if (i < corn) continue
+            dp[i] = Math.min(dp[i], dp[i - corn] + 1)
+        }
+    }
+    return dp[amount] === amount + 1 ? -1 : dp[amount]
+};
+// @lc code=end
+
